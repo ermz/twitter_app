cedde968c4927cfa7a1fb1ff6df859c880ca508b
Edson Ramirez <eramirez4369@gmail.com>
2019-01-02 22:55:30 -0500

    made changes to carrier_wave file

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,10 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJMDPPJFPZ5SSJADA'],
-      :aws_secret_access_key => ENV['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']
-    }
-    config.fog_directory     =  ENV['twitter-app-bucket']
-  end
-end
+#if Rails.env.production?
+#  CarrierWave.configure do |config|
+#    config.fog_credentials = {
+#      # Configuration for Amazon S3
+##      :aws_access_key_id     => ENV['AKIAJMDPPJFPZ5SSJADA'],
+  #    :aws_secret_access_key => ENV['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']
+  #  }
+  #  config.fog_directory     =  ENV['twitter-app-bucket']
+#  end
+#end
